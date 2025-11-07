05101b667ffc2dc4bc5c2591053ba3f9c1bf99cc
Samuel-Lucas <samuel.moreira10@outlook.com>
2025-11-06 21:48:45 -0300

    Adicionando mais colunas para tabela users

--- src/database/migrations/1762091916662-User.ts
+++ src/database/migrations/1762091916662-User.ts
@@ -11,6 +11,22 @@ export class User1762091916662 implements MigrationInterface {
                         name: 'id_user',
                         type: 'string',
                         isPrimary: true
+                    },
+                    {
+                        name: 'name',
+                        type: 'string',
+                        isNullable: false
+                    },
+                    {
+                        name: 'email',
+                        type: 'string',
+                        isNullable: false,
+                        isUnique: true
+                    },
+                    {
+                        name: 'password',
+                        type: 'string',
+                        isNullable: false
                     }
                 ]
             })
